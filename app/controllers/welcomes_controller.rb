2b14a1a5b09a34480cf376bd249f4544a8d8759e
ro <kronew6@gmail.com>
2022-01-12 10:39:48 -0600

    adds /about_path route

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,4 +2,8 @@ class WelcomesController < ApplicationController
   def hello_method
     render json: { message: "Greetings!" }
   end
+
+  def about_method
+    render json: { message: "My favorite computing language so far is Ruby!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/welcome_path", controller: "welcomes", action: "hello_method"
+
+  get "/about_path", controller: "welcomes", action: "about_method"
 end
